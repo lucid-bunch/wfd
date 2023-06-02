e0938a844b23831114b589935c380db59f3fcd28
Christian Abels <christian.abels1@ikea.com>
2023-06-02 17:33:00 +0200

    Simplify command execution

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,11 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/lucid-bunch/wfd/internal/adapter/ica"
+	"github.com/lucid-bunch/wfd/internal/store"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +15,82 @@ var rootCmd = &cobra.Command{
 	Use:   "wfd",
 	Short: "wfd is a tool that helps with dinner planning",
 	Long:  "What's For Dinner is a tool that helps with dinner planning",
+	Run: func(cmd *cobra.Command, args []string) {
+		cooldown := 4
+		delay := func() { time.Sleep(1 * time.Second) }
+
+		cPath := os.Getenv("COOLDOWN_PATH")
+		cStore := store.New(cPath)
+
+		fmt.Printf("- reading from recipe cooldown store\n")
+		cData, err := cStore.Read()
+		if err != nil {
+			fmt.Printf("Error reading from recipe cooldown store: %s\n\n", err)
+		}
+		delay()
+
+		fmt.Printf("- applying %d week recipe cooldown\n", cooldown)
+		var excludeIDs []string
+		switch {
+		case len(cData) > cooldown:
+			for i := len(cData) - cooldown; i < len(cData); i++ {
+				excludeIDs = append(excludeIDs, cData[i]...)
+			}
+		case len(cData) <= cooldown:
+			for _, row := range cData {
+				excludeIDs = append(excludeIDs, row...)
+			}
+		default:
+			excludeIDs = []string{}
+		}
+		delay()
+
+		bPath := os.Getenv("BLOCK_PATH")
+		bStore := store.New(bPath)
+
+		fmt.Printf("- reading from recipe block store\n")
+		bData, err := bStore.Read()
+		if err != nil {
+			fmt.Printf("Error reading from recipe block store: %s\n\n", err)
+		}
+		delay()
+
+		fmt.Printf("- applying blocked recipe filter\n")
+		for _, row := range bData {
+			excludeIDs = append(excludeIDs, row...)
+		}
+		delay()
+
+		fmt.Printf("- generating recipes")
+		svc := ica.NewService(os.Getenv("SEARCH_URL"), os.Getenv("TOKEN_URL"))
+		token := svc.AccessToken()
+
+		recipes := []ica.RecipeCard{
+			svc.RecipeCard(token, "/vegetarisk", excludeIDs...),
+			svc.RecipeCard(token, "/fisk", excludeIDs...),
+			svc.RecipeCard(token, "/kyckling", excludeIDs...),
+			svc.RecipeCard(token, "/vegetarisk", excludeIDs...),
+			svc.RecipeCard(token, "/kott", excludeIDs...),
+		}
+
+		fmt.Printf("\n- printing recipes\n\n")
+		ids := []string{}
+		for _, recipe := range recipes {
+			fmt.Printf("%s\n\n", recipe)
+			if recipe.ID != 0 {
+				ids = append(ids, fmt.Sprintf("%d", recipe.ID))
+			}
+		}
+		delay()
+
+		fmt.Printf("- writing to recipe store\n")
+		if err := cStore.Write(ids); err != nil {
+			fmt.Printf("Error writing to store: %s\n\n", err)
+		}
+		delay()
+
+		fmt.Printf("- done\n")
+	},
 }
 
 func init() {
@@ -19,7 +98,6 @@ func init() {
 		panic(err)
 	}
 	rootCmd.AddCommand(versionCmd)
-	rootCmd.AddCommand(weekCmd)
 }
 
 func Execute() {
